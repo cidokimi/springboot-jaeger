4710048bacd56cceef82adbd4fa10ac5975cd7f5
cidokimi <kadakoglucigdem@gmail.com>
2020-09-27 18:06:44 +0300

    Hello World

---
+++ jaeger/src/main/java/com/cidokimi/jaeger/JaegerApplication.java
@@ -0,0 +1,13 @@
+package com.cidokimi.jaeger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaegerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaegerApplication.class, args);
+	}
+
+}
