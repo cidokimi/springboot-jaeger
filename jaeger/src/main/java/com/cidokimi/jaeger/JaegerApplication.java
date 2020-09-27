e9d2a1dd85498979d654148238ce146cd8e730df
cidokimi <kadakoglucigdem@gmail.com>
2020-09-27 18:53:38 +0300

    Jaeger added

--- jaeger/src/main/java/com/cidokimi/jaeger/JaegerApplication.java
+++ jaeger/src/main/java/com/cidokimi/jaeger/JaegerApplication.java
@@ -2,9 +2,19 @@ package com.cidokimi.jaeger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.uber.jaeger.Configuration;
+import com.uber.jaeger.samplers.ProbabilisticSampler;
 
 @SpringBootApplication
 public class JaegerApplication {
+	
+	@Bean
+	public io.opentracing.Tracer jaegerTracer(){
+		return new Configuration("spring-boot",new Configuration.SamplerConfiguration(ProbabilisticSampler.TYPE,1),
+		new Configuration.ReporterConfiguration()).getTracer();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JaegerApplication.class, args);
