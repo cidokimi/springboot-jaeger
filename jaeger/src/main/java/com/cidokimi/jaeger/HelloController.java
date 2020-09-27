9e69724a19d90784338e833cef9182435a6e2f98
cidokimi <kadakoglucigdem@gmail.com>
2020-09-27 21:00:53 +0300

    Span added

--- jaeger/src/main/java/com/cidokimi/jaeger/HelloController.java
+++ jaeger/src/main/java/com/cidokimi/jaeger/HelloController.java
@@ -1,14 +1,31 @@
 package com.cidokimi.jaeger;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HelloController {
 	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	@RequestMapping("/hello")
 	public String hello() {
 		return "Hello World!";
+	}
+	
+	@RequestMapping("/span")
+	public String span() {
+		return "Hello from span";
+	}
+	
+	@RequestMapping("/spanTospan")
+	public String spanTospan() {
+		ResponseEntity<String> responseEntity = restTemplate.getForEntity("http://localhost:8081/hello",String.class);
+		return  responseEntity.getBody() +" from spanTospan";
 	}
 
 }
--- jaeger/src/main/java/com/cidokimi/jaeger/JaegerApplication.java
+++ jaeger/src/main/java/com/cidokimi/jaeger/JaegerApplication.java
@@ -2,7 +2,9 @@ package com.cidokimi.jaeger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 import com.uber.jaeger.Configuration;
 import com.uber.jaeger.samplers.ProbabilisticSampler;
@@ -16,6 +18,11 @@ public class JaegerApplication {
 		new Configuration.ReporterConfiguration()).getTracer();
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+		return restTemplateBuilder.build();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JaegerApplication.class, args);
 	}
